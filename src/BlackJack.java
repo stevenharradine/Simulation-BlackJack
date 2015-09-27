b23402291334b5829ed36b5adcf84c414a421d82
steven harradine <steven.harradine@telus.com>
2015-09-26 22:10:35 -0400

    removed old way, focusing on new

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -20,77 +20,23 @@
  public class BlackJack {
  	public static void main (String[] args) {
  		Scanner sc = new Scanner (System.in);	// input stream
- 
- 		int userCount;	// the value of all the users hand
- 		int dealCount;	// the value of all the dealers hand
- 		 		
- 		Hand userCards = new Hand(100);	// all cards delt to the user
- 		Hand dealCards = new Hand();	// all cards delt to the dealer
- 		
- 		int number_of_decks_in_shoe = 10;
- 		Shoe shoe = new Shoe (number_of_decks_in_shoe);
- 		int number_of_hands_to_play = 500;
  		
  		System.out.println ("\t\tBlack Jack Sim");
  		System.out.println ("\t\t~~~~~~~~~~~~~~");
- 		
- 		shoe.shuffle();	// shuffle the deck
- 		
- 		// print table header
- 		System.out.println ("My Hand\t\tDealers Hand");
- 		System.out.println ("~~~~~~~\t\t~~~~~~~~~~~~");
- 		System.out.println ("");
 
-		// hide the first card in the deck
-		shoe.dealCard();
-		
-		for (int a = 0; a < number_of_hands_to_play; a++) {
-			// place bet
-			if (userCards.getCount() > 10)
-				userCards.bet (15);
-			else if (userCards.getCount() > 0)
-				userCards.bet (10);
-			else
-				userCards.bet (5);
-			
-			
-			// deal user and dealer 1 card
-	 		userCards.addCard(shoe.dealCard());
-	 		dealCards.addCard(shoe.dealCard());
-	 		
-	 		// display table entry
-			System.out.println (userCards.getLastCard() + "\t" + dealCards.getLastCard());
-			
-			// deal user cards
-			do {
-				userCards.addCard (shoe.dealCard());
-				System.out.println (userCards.getLastCard());
-			} while (userCards.getHandValue() < 17);
 
-			// deal dealer cards
-			do {
-				dealCards.addCard (shoe.dealCard());
-				System.out.println ("\t\t" + dealCards.getLastCard());
-			} while (dealCards.getHandValue() < 17);
+ 		int    number_of_seats_at_the_table = 5;
+ 		int    number_of_decks_in_shoe      = 6;
 
-			// display footer
-	 		System.out.println ("");
-			System.out.println ("User: " + userCards.getHandValue() + "\tDealer: " + dealCards.getHandValue());
-			System.out.println ("Cash: " + userCards.getCash());
-			System.out.println ("\tCount: " + userCards.getCount());
-			System.out.println ("");
-			
-			// fix bets
-			System.out.println (userCards.isWinner (dealCards));
-			
-			// resets for new round
-			userCards.reset();
-			dealCards.reset();
+ 		Table  table       = new Table (
+ 			number_of_seats_at_the_table,
+ 			number_of_decks_in_shoe
+ 		);
 
-			if (shoe.needsShuffle()) {
-				shoe.shuffle();
-				userCards.resetCount();
-			}
-		}
+ 		Player basicPlayer = new Player ();
+
+ 		table.addPlayer (basicPlayer, 0);
+
+ 		table.play();
  	}
  }
--- src/Table.java
+++ src/Table.java
@@ -1,13 +1,23 @@
 public class Table {
-	private Dealer dealer;
-	private Seat[] seats;
+  private Dealer   dealer;
+	private Shoe     shoe;
+	private Player[] seats;
 
-	public Table (int number_of_seats) {
+	public Table (int number_of_seats, int number_of_decks_in_shoe) {
 		dealer = new Dealer ();
-		seats  = new Seat[number_of_seats];
-
-		for (Seat seat : seats) {
-			seat = new Seat ();
-		}
+    shoe   = new Shoe (number_of_decks_in_shoe);
+		seats  = new Player[number_of_seats];
 	}
+
+  public void addPlayer (Player player, int seatNumber) {
+    seats[seatNumber] = player;
+  }
+
+  public void play () {
+    placeBets ();
+  }
+
+  private void placeBets () {
+    
+  }
 }
